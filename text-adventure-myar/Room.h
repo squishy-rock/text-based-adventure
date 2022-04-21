644ef273cd8d123172487654d7480ebd37ea248f
squishy-rock <79102748+squishy-rock@users.noreply.github.com>
2022-04-20 22:42:54 -0400

    Clean up

--- text-adventure-myar/Room.h
+++ text-adventure-myar/Room.h
@@ -1,7 +1,16 @@
 #pragma once
+#include <string>
+
+using namespace std;
+
 class Room
 {
-private:
+public: //for all the subclasses to have access of variables and functions
+	string _name;
+	string _type;
+	string _description;
+	string _nextRoom;
+	virtual void LoadRoom(string fileName); //virtual denotes that any subclass denotes this function -> needed for subclass fumctionality. subclasses can define dif behaviour for this function
 
 };
 
